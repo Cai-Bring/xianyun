4a037631251cc1c20f7ff4ccba859932af3dcd34
Cai-Bring <743489214@qq.com>
2020-01-16 20:28:19 +0800

    公共页底部部分完成

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,14 +3,17 @@
     <Header />
     <!-- 内容占位组件 -->
     <nuxt />
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 };
 </script>
